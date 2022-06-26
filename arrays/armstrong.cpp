28058bdfefe520838a761eedf1a0294c8865d001
Raj Mourya <rajhmourya@gmail.com>
2022-06-26 19:39:29 +0530

    armstrong

---
+++ arrays/armstrong.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<math.h>
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+int main(){
+
+    int num;
+    cin >> num;
+
+    int n = 0;
+
+    int temp = num;
+
+    while(temp != 0){
+        temp = temp/10;
+        n++;
+    }
+
+    temp = num;
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        int prod = 1;
+        int t =  temp%10;
+        for(int j = 0; j < n; j++){
+            prod *=t;
+        }
+
+        sum += prod;
+        temp = temp/10;
+
+    }
+
+
+    if(sum == num){
+        cout<<"armstrong"<<endl;
+    }
+    else{
+        cout<<"not armstrong"<<endl;
+    }
+    return 0;
+}
