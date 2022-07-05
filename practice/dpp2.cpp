40e37dd4b8b0c26c7eff4360704b056875a6e0dd
Raj Mourya <rajhmourya@gmail.com>
2022-07-05 11:08:17 +0530

    practice pointers

---
+++ practice/dpp2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int* arr =  new int[2];
+    arr[0] = 1;
+    arr[1] = 2;
+    cout << arr[0] << " " << arr[1] <<endl;
+
+    int arr2[2];
+    arr2[0] = 1;
+    arr2[1] = 2;
+    cout << arr2[0] << " " << arr2[1]<<endl;
+
+    int *p = arr;
+    cout <<*(p+1)<<" "<<*(arr+1)<<endl;
+
+    int *p2 = arr2;
+    cout <<*(p2+1)<<" "<<*(arr2+1)<<endl;
+
+    // p++; p2++;
+    // cout << *(p)<<" "<<*(p2)<<endl;
+
+
+    int **q = &p;
+
+    cout << *(q)<<arr<<endl;
+    return 1;
+}
